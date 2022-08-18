8413b136483dea69f02258954f00372c85542c3e
Daniel Tulpa <dtulpa16@yahoo.com>
2022-08-18 11:03:59 -0500

    Added Robot + Weapon Class

---
+++ src/main/java/org/example/Robot.java
@@ -0,0 +1,13 @@
+package org.example;
+
+public class Robot {
+    public String name;
+    public int health;
+    public Weapon weapon;
+
+    public Robot(String name, int health, Weapon weapon) {
+        this.name = name;
+        this.health = health;
+        this.weapon = weapon;
+    }
+}
---
+++ src/main/java/org/example/Weapon.java
@@ -0,0 +1,13 @@
+package org.example;
+
+import java.util.Scanner;
+
+public class Weapon {
+    public String name;
+    public int attackPower;
+
+    public Weapon() {
+        Scanner sc = new Scanner(System.in);
+        
+    }
+}
